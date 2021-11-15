7318765a8b8ac64f4d1978c2082e19210b036ef9
Vergus866 <denzvergus37@gmail.com>
2021-11-15 11:29:28 +0300

    update

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ function SUBMIT() {
     var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   
     var dayOfBirth = (((century / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
+    console.log(date);
     var dayOfBirth = Math.floor(dayOfBirth);
   
     if (gender === "Male") {
